547830e29104e6c849049424dc5a62aa85ab069b
Lachlan Phillips <lachlan.phillips.00@gmail.com>
2021-08-16 09:08:17 +1000

    Edit probs

--- EAC_probs.py
+++ EAC_probs.py
@@ -7,16 +7,16 @@ from data_visulisation import *
 # Configuration #
 #################
 # directories
-ROMS_directory = '/Volumes/LP_MstrData/master-data/ocean/ROMS/Montague_subset'
+ROMS_directory = '/Volumes/LP_MstrData/master-data/ocean/ROMS/highres'
 # analysis zone box
 analysisBox = [149, 151, -37.2, -35] # EAC Jet
 # check study zone box position
 check_boxROMS([analysisBox], ROMS_directory, 
-            depthmax=4000, save=True, out_fn='./plots/ROMS_lowres.png', title='lowres',
+            depthmax=4000, save=True, out_fn='./plots/ROMS_highres.png', title='highres',
             zoom2box=True)
 
 # build EAC LR model
 lr_model = current_model('./data/training_data.csv')
 
 # get count data for regions
-CC_probs(ROMS_directory, lr_model, analysisBox, years=[2012, 2014], out_fn='./output/EACprob.csv')
+CC_probs(ROMS_directory, lr_model, analysisBox, years=[2012, 2014], out_fn='./output/EACprob_highres.csv')
